92e5193d9e7a8924c14d27958d4e733eb6e2893c
JY1330 <55523921+JY1330@users.noreply.github.com>
2020-09-24 17:06:25 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            //string ipt;
+            Console.WriteLine("Hello CSharp World!");
+            Console.WriteLine("첫번째 정수 입력 : ");
+            /*
+            ipt = Console.ReadLine();
+            num1 = int.Parse(ipt);
+            */
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("두번째 정수 입력 : ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1 + "+" + num2 + "=" + (num1 + num2));
+            Console.WriteLine(num1 + "-" + num2 + "=" + (num1 - num2));
+            Console.WriteLine(num1 + "*" + num2 + "=" + (num1 * num2));
+            Console.WriteLine(num1 + "/" + num2 + "=" + (num1 / num2));
+        }
+    }
+}
